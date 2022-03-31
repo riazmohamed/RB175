9594971bed8c678456cc3249ac581283b623e8da
riazmohamed <railsdevriaz@gmail.com>
2022-03-31 12:03:35 +0100

    Create a TCP server

--- simple_server/server.rb
+++ simple_server/server.rb
@@ -5,11 +5,9 @@ loop do
   client = server.accept
 
   request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
   puts request_line
 
   client.puts request_line
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
-  client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
   client.close
 end
