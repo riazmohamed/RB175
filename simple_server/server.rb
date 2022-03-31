ea209ef4ced5a998c5c6917221286666ec20fa72
riazmohamed <railsdevriaz@gmail.com>
2022-03-31 22:50:10 +0100

    refractor #parse_request

--- simple_server/server.rb
+++ simple_server/server.rb
@@ -1,17 +1,17 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
 
-# def parse_request(request_line)
-#   http_method, path_and_params, http = request_line.split(" ")
-#   path, params = path_and_params.split("?")
-#
-#   params = params.split("&").each_with_object({}) do |pair, hash|
-#     key, value = pair.split("=")
-#     hash[key] = value
-#   end
-#
-#   [http_method, path, params]
-# end
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
 
 server = TCPServer.new("localhost", 3003)
 loop do
@@ -50,13 +50,7 @@ loop do
   # client.puts "</body>"
   # client.puts "</html>"
 
-  http_method, path_and_params, http = request_line.split(" ")
-
-  path, params = path_and_params.split("?")
-  params = params.split("&").each_with_object({}) do |pair, hash|
-    key, value = pair.split("=")
-    hash[key] = value
-  end
+  http_method, path, params = parse_request(request_line)
 
   client.puts request_line
   client.puts http_method
