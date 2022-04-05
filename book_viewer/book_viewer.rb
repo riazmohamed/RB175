b1d84164e90ba62ea6638f852f515beb61d00776
riazmohamed <railsdevriaz@gmail.com>
2022-04-05 13:31:08 +0100

    add search functionality

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -6,6 +6,14 @@ helpers do
   def in_paragraphs(str)
     str.split("\n\n").map { |para| "<p>#{para}<\p>" }.join
   end
+
+  def split_into_words(str, word)
+    str.split(/[ .]/).map(&:downcase).any? { |string| string.match(/#{word}/)}
+  end
+end
+
+not_found do
+  redirect '/'
 end
 
 before do
@@ -21,9 +29,22 @@ end
 get "/chapters/:number" do
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
 
-  @chapter = File.read("data/chp#{params['number']}.txt")
+  redirect '/' unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/search" do
+  @value = params['query']
+
+  @arr = (@contents.select.with_index do |chapter, idx|
+    @chapter = File.read("data/chp#{idx + 1}.txt")
+    split_into_words(@chapter, @value)
+  end)
+
+  erb :search
 end
