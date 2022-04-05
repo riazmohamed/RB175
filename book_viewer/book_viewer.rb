2884bb0c5d60c420373ea29d8f765015683b78b3
riazmohamed <railsdevriaz@gmail.com>
2022-04-05 08:43:16 +0100

    Add in_paragraph view helper method to arrange the paragraphs in the view

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,16 +2,23 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+helpers do
+  def in_paragraphs(str)
+    str.split("\n\n").map { |para| "<p>#{para}<\p>" }.join
+  end
+end
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
